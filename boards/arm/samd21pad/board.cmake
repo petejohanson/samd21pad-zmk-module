21c8f050a20f9380e64d71095864238a4370fed9
Peter Johanson <peter@peterjohanson.com>
2024-05-30 12:37:11 -0700

    Fix up various defaults for flashing UF2, name, etc.

--- boards/arm/samd21pad/board.cmake
+++ boards/arm/samd21pad/board.cmake
@@ -1,5 +1,8 @@
 # Copyright (c) 2020 Google LLC.
 # SPDX-License-Identifier: Apache-2.0
 
+board_runner_args(jlink "--device=ATSAMD21E18A" "--speed=4000")
 include(${ZEPHYR_BASE}/boards/common/bossac.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/uf2.board.cmake)
 include(${ZEPHYR_BASE}/boards/common/openocd.board.cmake)
+include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
